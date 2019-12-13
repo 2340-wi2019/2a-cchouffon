02a3e412956f1e4a359295a586ae73da30b4300e
cchouffon <58453159+cchouffon@users.noreply.github.com>
2019-12-13 09:10:40 -0600

    New

---
+++ js/Ass.js
@@ -0,0 +1,33 @@
+$(document).ready (function(){
+
+
+});
+
+function clem(){
+  var female1 = prompt("Please enter a female's name (1 of 2)");
+  document.getElementById("female1-1").innerHTML = female1;
+  var female2 = prompt("Please enter a female's name (2 of 2)");
+  document.getElementById("female2").innerHTML = female2;
+  var male1 = prompt("Please enter a male's name");
+  document.getElementById("male1-1").innerHTML = male1;
+  var state1 = prompt("Please enter a state name");
+  document.getElementById("state1").innerHTML = state1;
+  var occupation1 = prompt("Please enter the name of an occupation");
+  document.getElementById("occupation1").innerHTML = occupation1;
+  var noun1 = prompt("Please enter a noun (1 of 5)");
+  document.getElementById("noun1").innerHTML = noun1;
+  var noun2 = prompt("Please enter a noun (2 of 5)");
+  document.getElementById("noun2").innerHTML = noun2;
+  var noun3 = prompt("Please enter a noun (3 of 5)");
+  document.getElementById("noun3").innerHTML = noun3;
+  var noun4 = prompt("Please enter a noun (4 of 5)");
+  document.getElementById("noun4").innerHTML = noun4;
+  var noun5 = prompt("Please enter a noun (5 of 5)");
+  document.getElementById("noun5").innerHTML = noun5;
+  var color1 = prompt("Please enter the name of a color");
+  document.getElementById("color1-1").innerHTML = color1;
+  var feature1 = prompt("Please enter the name of a feature");
+  document.getElementById("feature1-1").innerHTML = feature1;
+  
+
+}

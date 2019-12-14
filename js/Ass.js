82725b3d789f8cc02f8168211347d9216fdb9623
cchouffon <58453159+cchouffon@users.noreply.github.com>
2019-12-14 13:38:39 -0600

    New

--- js/Ass.js
+++ js/Ass.js
@@ -1,33 +1,35 @@
 $(document).ready (function(){
 
-
-});
-
-function clem(){
-  var female1 = prompt("Please enter a female's name (1 of 2)");
-  document.getElementById("female1-1").innerHTML = female1;
+    var female1 = prompt("Please enter a female's name (1 of 2)");
+  $('#female1-1').text(female1);
+  $('#female1-2').text(female1);
+  $('#female1-3').text(female1);
   var female2 = prompt("Please enter a female's name (2 of 2)");
-  document.getElementById("female2").innerHTML = female2;
+  $('#female2-1').text(female2);
+  $('#female2-2').text(female2);
   var male1 = prompt("Please enter a male's name");
-  document.getElementById("male1-1").innerHTML = male1;
+  $('#male1-1').text(male1);
+  $('#male1-2').text(male1);
   var state1 = prompt("Please enter a state name");
-  document.getElementById("state1").innerHTML = state1;
+  $('#state1').text(state1);
   var occupation1 = prompt("Please enter the name of an occupation");
-  document.getElementById("occupation1").innerHTML = occupation1;
+  $('#occupation1').text(occupation1);
   var noun1 = prompt("Please enter a noun (1 of 5)");
-  document.getElementById("noun1").innerHTML = noun1;
+  $('#noun1').text(noun1);
   var noun2 = prompt("Please enter a noun (2 of 5)");
-  document.getElementById("noun2").innerHTML = noun2;
+  $('#noun2').text(noun2);
   var noun3 = prompt("Please enter a noun (3 of 5)");
-  document.getElementById("noun3").innerHTML = noun3;
+  $('#noun3').text(noun3);
   var noun4 = prompt("Please enter a noun (4 of 5)");
-  document.getElementById("noun4").innerHTML = noun4;
+  $('#noun4').text(noun4);
   var noun5 = prompt("Please enter a noun (5 of 5)");
-  document.getElementById("noun5").innerHTML = noun5;
+  $('#noun5').text(noun5);
   var color1 = prompt("Please enter the name of a color");
-  document.getElementById("color1-1").innerHTML = color1;
+  $('#color1-1').text(color1);
+  $('#color1-2').text(color1);
+  $('#color1-3').text(color1);
+  $('#color1-4').text(color1);
   var feature1 = prompt("Please enter the name of a feature");
-  document.getElementById("feature1-1").innerHTML = feature1;
-  
-
-}
+  $('#feature1-1').text(feature1);
+  $('#feature1-2').text(feature1);
+});
